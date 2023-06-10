8903359582faeea72e4e07ee2a8604c7406ad9a2
IdanPelled <pelled.idan@gmail.com>
2023-06-10 21:46:38 +0300

    add log

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class eventlet -w 1 --chdir ./breeze run:app
+web: gunicorn --worker-class eventlet -w 1 run:app
--- run.py
+++ run.py
@@ -1,3 +1,9 @@
+import logging
+import os
+
+log = logging.getLogger(__name__)
+log.info(os.getcwd())
+
 from breeze.factory import create_app, socketio
 
 
