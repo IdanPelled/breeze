90b5cfbcd1552f1dd1342d1346806c91f814461f
IdanPelled <pelled.idan@gmail.com>
2023-06-25 18:06:30 +0300

    add remote.sh script

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn -k eventlet -w 1 run:app
+web: ./remote.sh && gunicorn -k eventlet -w 1 run:app
--- local.sh
+++ local.sh
@@ -4,9 +4,8 @@ make
 cd ..
 
 # Run the server
-cd ./server
 pip install -r requirements.txt
 export FLASK_APP=run.py
-export FLASK_DEBUG=1 
+export FLASK_DEBUG=1
 export TEMPLATES_AUTO_RELOAD=1
 flask run -h "0.0.0.0"
