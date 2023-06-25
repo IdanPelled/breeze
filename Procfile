cbb0951f37b2747ef6e6e287552a25cda1f91c5e
IdanPelled <pelled.idan@gmail.com>
2023-06-25 16:50:12 +0300

    edit heroku config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn --worker-class eventlet -w 1 run:app
+web: gunicorn -k eventlet -w 1 run:app
--- run.py
+++ run.py
@@ -2,7 +2,9 @@ from factory import create_app, socketio
 from log import setup_logger
 
 
-# The main entry point of the application.
 app = create_app()
 setup_logger(app)
-socketio.run(app)
+
+
+if __name__ =="__main__":
+    socketio.run(app)
